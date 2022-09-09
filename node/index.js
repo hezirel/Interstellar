624c8d80d6b163c266e7060ceaabac34d7de2469
hezirel <80560775+hezirel@users.noreply.github.com>
2022-09-10 01:54:30 +0200

    added GET route

--- node/index.js
+++ node/index.js
@@ -1,6 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
+const route = require('koa-route');
 const port = process.env.PORT || 3000;
+
+app.use(route.get('/', (ctx) => {
+    ctx.body = 'Hello world';
+}));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
