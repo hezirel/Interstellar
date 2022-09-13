c86d075d977ae793dd7f2d0ce663a2b23244a96d
hezirel <80560775+hezirel@users.noreply.github.com>
2022-09-13 04:26:37 +0200

    Moving variables to .env file for easier docker config

--- node/server.js
+++ node/server.js
@@ -3,15 +3,17 @@ import logger from 'koa-logger';
 import { ApolloServer } from 'apollo-server-koa';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { typeDefs, resolvers } from './graphql/index.js';
+import Knex from 'knex';
 
-const port = process.env.PORT;
+const pg_url = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@mimir:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+const apollo_port = process.env.APOLLO_PORT || 4000;
 const app = new Koa();
 
-//const knex = require('knex')({
-    //client: 'pg',
-    //connection: "postgres://postgres:gnosis@mimir:5432/interstellar",
-    //searchPath: ['knex', 'public']
-//});
+const knex = new Knex({
+    client: 'pg',
+    connection: pg_url,
+    searchPath: ['knex', 'public']
+});
 
 const server = new ApolloServer({
     typeDefs,
@@ -22,10 +24,15 @@ const server = new ApolloServer({
 });
 
 app.use(logger());
+//_#:Split into async module
 await server.start();
 
 server.applyMiddleware({ app });
 
-app.listen({ port }, () => {
-    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+app.listen( apollo_port , () => {
+    console.log(`🚀 Server ready at http://localhost:${apollo_port}${server.graphqlPath}`);
+});
+
+knex.raw("SELECT 1").then((res) => {
+    console.log(res);
 });
